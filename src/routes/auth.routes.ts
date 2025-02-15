cfecbd4d55ec5656c009066d1d65304688fdc96e
yasg-uru <yashpawar12122004@gmail.com>
2025-02-15 23:15:44 +0530

    corrected

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -26,7 +26,7 @@ authRouter.get(
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry
     });
     
-    // res.redirect("http://localhost:5173/");
+   
     res.redirect(process.env.CLIENT_URL as string);
   }
 );
